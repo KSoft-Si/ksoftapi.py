13bb56da1b9fbbb9f2634f6e12b2e404c1e1da7b
Devoxin <lukel9914@gmail.com>
2020-02-13 20:21:14 +0000

    More typings!

--- ksoftapi/models.py
+++ ksoftapi/models.py
@@ -1,38 +1,38 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
 class BanInfo:
     def __init__(self, **kwargs):
-        self.id = kwargs.get('id')
-        self.name = kwargs.get('name')
-        self.discriminator = kwargs.get('discriminator')
-        self.moderator_id = kwargs.get('moderator_id')
-        self.reason = kwargs.get('reason')
-        self.proof = kwargs.get('proof')
-        self.is_ban_active = kwargs.get('is_ban_active')
-        self.can_be_appealed = kwargs.get('can_be_appealed')
-        self.timestamp = kwargs.get('timestamp')
-        self.appeal_reason = kwargs.get('appeal_reason')
-        self.appeal_date = kwargs.get('appeal_date')
-        self.requested_by = kwargs.get('requested_by')
-        self.exists = kwargs.get('exists')
+        self.id: str = kwargs.get('id')
+        self.name: str = kwargs.get('name')
+        self.discriminator: str = kwargs.get('discriminator')
+        self.moderator_id: str = kwargs.get('moderator_id')
+        self.reason: str = kwargs.get('reason')
+        self.proof: str = kwargs.get('proof')
+        self.is_ban_active: bool = kwargs.get('is_ban_active')
+        self.can_be_appealed: bool = kwargs.get('can_be_appealed')
+        self.timestamp: str = kwargs.get('timestamp')
+        self.appeal_reason: Optional[str] = kwargs.get('appeal_reason')
+        self.appeal_date: Optional[str] = kwargs.get('appeal_date')
+        self.requested_by: str = kwargs.get('requested_by')
+        self.exists: bool = kwargs.get('exists')
 
 
 class BanSimple:
     def __init__(self, **kwargs):
-        self.id = kwargs.get("id")
-        self.reason = kwargs.get("reason")
-        self.proof = kwargs.get("proof")
-        self.moderator_id = kwargs.get("moderator_id")
-        self.active = kwargs.get("active")
+        self.id: str = kwargs.get('id')
+        self.reason: str = kwargs.get('reason')
+        self.proof: str = kwargs.get('proof')
+        self.moderator_id: str = kwargs.get('moderator_id')
+        self.active: bool = kwargs.get('is_ban_active')
 
 
 class Image:
     def __init__(self, **kwargs):
-        self.snowflake = kwargs.get('snowflake')
-        self.url = kwargs.get('url')
-        self.nsfw = kwargs.get('nsfw')
-        self.tag = kwargs.get('tag')
+        self.url: str = kwargs.get('url')
+        self.snowflake: str = kwargs.get('snowflake')
+        self.nsfw: bool = kwargs.get('nsfw')
+        self.tag: str = kwargs.get('tag')
 
 
 class LyricResult:
@@ -53,14 +53,14 @@ class LyricResult:
 
 class PaginatorListing:
     def __init__(self, **kwargs):
-        self.count = kwargs.get('ban_count')
-        self.page_count = kwargs.get('page_count')
-        self.per_page = kwargs.get('per_page')
-        self.page = kwargs.get('page')
-        self.on_page = kwargs.get('on_page')
-        self.next_page = kwargs.get('next_page')
-        self.previous_page = kwargs.get('previous_page')
-        self.data = [BanInfo(**ban) for ban in kwargs.get('data')]
+        self.count: int = kwargs.get('ban_count')
+        self.page_count: int = kwargs.get('page_count')
+        self.per_page: int = kwargs.get('per_page')
+        self.page: int = kwargs.get('page')
+        self.on_page: int = kwargs.get('on_page')
+        self.next_page: Optional[int] = kwargs.get('next_page')
+        self.previous_page: Optional[int] = kwargs.get('previous_page')
+        self.data: List[BanInfo] = [BanInfo(**ban) for ban in kwargs.get('data')]
 
 
 class Recommendation:
@@ -91,22 +91,22 @@ class Recommendation:
 
 class RedditImage:
     def __init__(self, **kwargs):
-        self.title = kwargs.get('title')
-        self.image_url = kwargs.get('image_url')
-        self.url = self.image_url
-        self.source = kwargs.get('source')
-        self.subreddit = kwargs.get('subreddit')
-        self.upvotes = kwargs.get('upvotes')
-        self.downvotes = kwargs.get('downvotes')
-        self.comments = kwargs.get('comments')
-        self.created_at = kwargs.get('created_at')
-        self.nsfw = kwargs.get('nsfw')
+        self.author: str = kwargs.get('author')
+        self.title: str = kwargs.get('title')
+        self.image_url: str = kwargs.get('image_url')
+        self.source: str = kwargs.get('source')
+        self.subreddit: str = kwargs.get('subreddit')
+        self.upvotes: int = kwargs.get('upvotes')
+        self.downvotes: int = kwargs.get('downvotes')
+        self.comments: int = kwargs.get('comments')
+        self.created_at: int = kwargs.get('created_at')
+        self.nsfw: bool = kwargs.get('nsfw')
 
 
 class Tag:
     def __init__(self, **kwargs):
-        self.name = kwargs.get('name')
-        self.nsfw = kwargs.get('nsfw')
+        self.name: str = kwargs.get('name')
+        self.nsfw: bool = kwargs.get('nsfw')
 
     def __str__(self):
         return self.name
@@ -114,16 +114,16 @@ class Tag:
 
 class TagCollection:
     def __init__(self, **kwargs):
-        self._raw = kwargs.get('models')
-        self.models = [Tag(**t) for t in kwargs.get('models')]
-        self.sfw_tags = kwargs.get('tags')
-        self.nsfw_tags = kwargs.get('nsfw_tags', [])
+        self.raw_models: List[dict] = kwargs.get('models')
+        self.models: List[Tag] = [Tag(**t) for t in self.raw_models]
+        self.sfw_tags: List[str] = kwargs.get('tags')
+        self.nsfw_tags: List[str] = kwargs.get('nsfw_tags', [])
 
     def __len__(self):
         return len(self.models)
 
     def __dict__(self):
-        return self._raw
+        return self.raw_models
 
     def __getitem__(self, item):
         return next((item == t.name for t in self.models), None)
@@ -141,7 +141,7 @@ class TagCollection:
 
 class WikiHowImage:
     def __init__(self, **kwargs):
-        self.url = kwargs.get('url')
-        self.title = kwargs.get('title')
-        self.nsfw = kwargs.get('nsfw')
-        self.article_url = kwargs.get('article_url')
+        self.url: str = kwargs.get('url')
+        self.title: str = kwargs.get('title')
+        self.nsfw: bool = kwargs.get('nsfw')
+        self.article_url: str = kwargs.get('article_url')
