5f5921d780fb35907a308d00b1f60ce2c9620767
EpicShardGaming YT <35035079+EpicShardGamingYT@users.noreply.github.com>
2020-02-14 18:26:25 +0100

    Clean setup.py and change licence to be GNU in __init__.py

--- ksoftapi/__init__.py
+++ ksoftapi/__init__.py
@@ -6,7 +6,7 @@ KSoft.Si API Wrapper with discord.py integration
 
 __title__ = 'ksoftapi'
 __author__ = 'AndyTempel'
-__license__ = 'MIT'
+__license__ = 'GNU'
 __copyright__ = 'Copyright 2018-2020 AndyTempel'
 __version__ = '0.2.0a'
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import re
+import ksoftapi
 
 from setuptools import setup
 
@@ -9,34 +9,30 @@ def get_requirements():
     return requirements
 
 
-version = ''
-with open('ksoftapi/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+version = ksoftapi.__version__
 
 if not version:
     raise RuntimeError('Version is not set')
 
-readme = ''
 with open('README.md') as f:
     readme = f.read()
 
-print(readme)
 setup(
     name='ksoftapi',
     packages=['ksoftapi'],
     version=version,
     description='KSoft.Si API Wrapper, customised for use in discord.py',
-    long_description=str(readme),
+    long_description=readme,
     author='AndyTempel',
-    author_email='andraz@korenc.eu',
+    author_email='support@ksoft.si',
     url='https://github.com/KSoft-Si/ksoftapi.py',
     download_url=f'https://github.com/KSoft-Si/ksoftapi.py/archive/{version}.tar.gz',
     keywords=['ksoftapi'],
     include_package_data=True,
     install_requires=get_requirements(),
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
